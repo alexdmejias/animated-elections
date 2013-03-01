17b3b3acada71e90e460b150a98be05037ec2000
alex mejias <amejias101@qc.cuny.edu>
2013-03-01 01:54:48 -0500

    fixed last path issue

--- assets/js/collections/elections.js
+++ assets/js/collections/elections.js
@@ -1,7 +1,7 @@
 define(['backbone', 'assets/js/models/election.js'], function(Backbone, Election) {
     var Elections = Backbone.Collection.extend({
         model: Election,
-        url: '/assets/data.json'
+        url: 'assets/data.json'
     });
 
     return Elections;
