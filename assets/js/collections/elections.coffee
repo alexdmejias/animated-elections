ffd2bb30b13c31ae8f83cdeb451fa17f92ffe2cd
alex mejias <amejias101@qc.cuny.edu>
2013-03-05 15:04:16 -0500

    optimized json file

--- assets/js/collections/elections.coffee
+++ assets/js/collections/elections.coffee
@@ -1,7 +1,7 @@
 define ['backbone', 'assets/js/models/election.js'], (Backbone, Election) ->
 	Elections = Backbone.Collection.extend(
 		model: Election
-		url: 'assets/data.json'
+		url: 'assets/data.min.json'
 	)
 
 	Elections
--- assets/js/collections/elections.js
+++ assets/js/collections/elections.js
@@ -4,7 +4,7 @@ define(['backbone', 'assets/js/models/election.js'], function(Backbone, Election
   var Elections;
   Elections = Backbone.Collection.extend({
     model: Election,
-    url: 'assets/data.json'
+    url: 'assets/data.min.json'
   });
   return Elections;
 });
