f88f6c9fe20dbcfc08a0751597a2cb5f1b86f7d1
alex mejias <amejias101@qc.cuny.edu>
2013-03-06 23:46:31 -0500

    switched to lodash, a lightter alternative to underscore

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,8 @@ require.config({
     paths: {
         // jquery: 'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min',
         jquery: 'components/jquery/jquery.min',
-        underscore: 'components/underscore/underscore-min',
+        // underscore: 'components/underscore/underscore-min',
+        underscore: 'components/lodash/lodash.custom.min',
         backbone: 'components/backbone/backbone-min',
         jqueryui: 'components/jqueryui/jquery-ui-1.10.1.custom.min'
     },
